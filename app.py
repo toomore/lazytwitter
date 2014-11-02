265adc791dba416ebfcd293bfc567c15dbaaa678
Toomore Chiang <toomore0929@gmail.com>
2014-11-02 13:55:36 +0800

    Add authenticate_url.

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ from requests_oauthlib import OAuth1Session
 
 
 app = Flask(__name__)
-oauth_session = OAuth1Session(
-        setting.client_key,
-        client_secret=setting.client_secret,
-        callback_uri=setting.callback_uri)
 
 @app.route("/")
 def home():
@@ -18,11 +14,19 @@ def home():
 
 @app.route("/twitter")
 def twitter_url():
+    oauth_session = OAuth1Session(
+            setting.client_key,
+            client_secret=setting.client_secret,
+            callback_uri=setting.callback_uri)
     oauth_session.fetch_request_token(setting.request_token_url)
-    return u'<a href="%(url)s">%(url)s</a>' % dict(url=oauth_session.authorization_url(setting.authorization_url))
+    return u'<a href="%(url)s">%(url)s</a>' % dict(url=oauth_session.authorization_url(setting.authenticate_url))
 
 @app.route("/twitter_back")
 def twitter_back():
+    oauth_session = OAuth1Session(
+            setting.client_key,
+            client_secret=setting.client_secret,
+            callback_uri=setting.callback_uri)
     oauth_session.parse_authorization_response(request.url)
     return u'%s' % oauth_session.fetch_access_token(setting.access_token_url)
 
